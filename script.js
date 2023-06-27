7c5bf368d0bf973a471eebe2cadde2ed49af6569
Shweta Pal <shwetapal975@gmail.com>
2023-06-27 23:04:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // complete this js code
 function Person(name, age) {
-  this.name = name;
-  this.age = age;
+  this._name = name;
+  this._age = age;
 }
 
 Person.prototype.greet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`);
 };
 
 function Employee(name, age, jobTitle) {
@@ -17,7 +17,7 @@ Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`);
 };
 
 
